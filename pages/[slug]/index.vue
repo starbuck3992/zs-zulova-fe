39f96e39115f667cdb2c329e1cb1b503c1153c73
Martin Cvešpr <cvespr@xdent.cz>
2023-10-28 11:57:12 +0200

    test nested slug

---
+++ pages/[slug]/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <div>
+      Dynamic Section page page
+      <pre>{{ $route.params }}</pre>
+    </div>
+    <NuxtLink to="/">Back to Home page</NuxtLink>
+  </div>
+</template>
