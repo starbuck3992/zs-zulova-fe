50970aa583192f519d5fa1fea093ac8663b2d3b3
Martin Cvešpr <cvespr@xdent.cz>
2023-11-05 19:39:33 +0100

    update page detail

--- pages/[slug].vue
+++ pages/[slug].vue
@@ -12,6 +12,7 @@
 </template>
 
 <script setup lang="ts">
+const { $directus, $readItems } = useNuxtApp()
 import { useRoute } from 'vue-router';
 const route = useRoute();
 
@@ -19,7 +20,11 @@ console.log('slug page',route.params.slug)
 const { data: page } = await useAsyncData('pages', () => {
   return $directus.request($readItems('pages', {
     filter: { slug: { _eq: route.params.slug }
-    }
+    },
+    fields: [
+      'id, title, content',
+      'article_id.*',
+    ]
   }))
 })
 </script>
