b3285e25c1dfffbc51915bd43e83ac3fefc78615
Martin Cvešpr <cvespr@xdent.cz>
2023-11-05 19:42:01 +0100

    add article

---
+++ pages/[section]/[slug].vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div>
+      Dynamic Slug page
+      <pre>{{ $route.params }}</pre>
+      <pre>
+        {{ article }}
+      </pre>
+    </div>
+    <NuxtLink to="/">Back to Home page</NuxtLink>
+  </div>
+</template>
+
+<script setup lang="ts">
+const { $directus, $readItems } = useNuxtApp()
+import { useRoute } from 'vue-router';
+const route = useRoute();
+
+console.log('slug page',route.params.slug)
+const { data: article } = await useAsyncData('articles', () => {
+  return $directus.request($readItems('articles', {
+    filter: { slug: { _eq: route.params.slug }
+    },
+    fields: [
+      'id, title, content',
+    ]
+  }))
+})
+</script>
