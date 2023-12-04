ee2c31333a3cb5cd9ddaf2b29228a580fb5bdfe0
Martin Cvešpr <cvespr@xdent.cz>
2023-12-04 16:55:52 +0100

    fix modal

--- components/Dialog.vue
+++ components/Dialog.vue
@@ -1,15 +1,20 @@
 <template>
-    <Dialog v-model:visible="dialog.is_open_modal" modal :header="dialog.title" :style="{ width: '50rem' }"
-        :breakpoints="{ '1199px': '75vw', '575px': '90vw' }">
+    <Dialog v-if="dialog" v-model:visible="dialog.is_open_modal" modal :header="dialog.title" :style="{ width: '50rem' }" :breakpoints="{ '1199px': '75vw', '575px': '90vw' }">
         <p v-html="dialog.content"></p>
     </Dialog>
 </template>
 <script setup lang="ts">
 import Dialog from 'primevue/dialog';
+import {onMounted} from "vue";
 
 const { $directus, $readItems } = useNuxtApp()
-const { data: dialog } = await useAsyncData('modal', () => {
-  return $directus.request($readItems('modal', {}))
+
+const dialog = ref();
+
+onMounted(async () => {
+  dialog.value = await $directus.request($readItems('modal', {
+    fields: ['id, title, content, is_open_modal']
+  }))
 })
 
 </script>
